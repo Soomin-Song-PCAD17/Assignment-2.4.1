a3cc8410b7e2730744b9120e2b7b12f690c78e47
Soomin Song <song.soomin.418@outlook.com>
2025-04-15 10:57:00 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/// 1. Write a program in C# Sharp to find the sum of all array elements.
+/// Test Data :
+/// Input the number of elements to be stored in the array :3
+/// Input 3 elements in the array :
+/// element - 0 : 2
+/// element - 1 : 5
+/// element - 2 : 8
+/// Expected Output :
+/// Sum of all elements stored in the array is : 15
+
+Console.Write("Input the number of elements to be stored in the array : ");
+double[] array = new double[Convert.ToInt32(Console.ReadLine())];
+double sum = 0;
+for(int i=0;i<array.Length;i++) {
+    Console.Write($"element - {i} : ");
+    array[i] = Convert.ToDouble(Console.ReadLine());
+    sum += array[i];
+}
+
+Console.WriteLine($"Sum of all elements stored in the array is : {sum}");
